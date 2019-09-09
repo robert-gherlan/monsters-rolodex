7d4f8815f0d42c24fff237ae2c5fd60c85ec2ba6
Robert1994A <UVkcA-gc!_M:WL4>
2019-09-09 23:21:39 +0300

    Updates

---
+++ precache-manifest.fc184a13e9ca654d2c85de445de59c10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11bb9a798882e47f3814bf39de1c967",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43b9f468df8b1a2547b0",
+    "url": "/monsters-rolodex/static/css/main.191bb1b5.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "43b9f468df8b1a2547b0",
+    "url": "/monsters-rolodex/static/js/main.cd55b9eb.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
